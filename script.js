575666db355cdd709e938c5829bdd5af4f3c5e35
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2024-12-17 13:11:18 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// chiedi utente km del viaggio e eta
+
+// calcola km x 0.21€ al km
+
+// SE eta<18 applica 20%
+
+// SE eta>65 applica 40%
+
+// SE 18>eta>65 no sconto
+
+// fai output con prezzo finale
